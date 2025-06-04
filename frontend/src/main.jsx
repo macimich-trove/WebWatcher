2d5ee4b0452bcbb13356495262d5443f3abaaf84
macimich-trove <michael.maciel@protonmail.com>
2025-06-04 01:31:27 -0400

    UpToDate

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Dashboard from './Dashboard.jsx'
 
 import { Provider } from 'react-redux'
 // import store from '' ./store
@@ -22,6 +23,7 @@ createRoot(document.getElementById('root')).render(
 
 <Route path="/" element={<App/>}/>
 
+//<Route path="/dashboard" element={<Dashboard/>}/>
 
 </Routes>
 
--- middleware/logger-middleware.js
+++ middleware/logger-middleware.js
@@ -1,7 +1,4 @@
 const express = require('express');
-//const app = require('../index.js');
-const app = express();
-
 const cors = require('cors');
 const morganlogs = require('morgan');
 const Stream = require('stream');
@@ -18,69 +15,20 @@ const Stream = require('stream');
 
 /*
 const RequestMethodsLog = [];
-app.get('/stats',(req, res) => {
-//Send route stats and JSON payload
-//const PageHtml= RequestMethodsLog.map((x)=>x).join(' ');
-const PageHtml= RequestMethodsLog.map((Request) => `<li><b>${Request}</b></li>`).join(' ');
-console.log("#STATS ROUTE#");
-
-(() => {
-res.write(`<h1><b>#STATS ROUTE#</b></h1>`);
-})(); 
-
-res.write(PageHtml);
-
-});
-*/
-
-
-
-const RequestMethodsLog = [];
-//app.use(express.json()); 
-
-
-
-const Logger = function (req, res, next) {
-
-const PageHtml= RequestMethodsLog.map((Request) => `<li><b>${Request}</b></li>`).join(' ');
-console.log("#STATS ROUTE#");
-(() => {
-res.write(`<h1><b>#STATS ROUTE#</b></h1>`);
-})();
-
-res.write(PageHtml);
-
-
-
 const StreamObject = new Stream.Writable({
     write:function(chunk, encoding, callback) {
 try{
    console.log(chunk.toString());
    console.log("Writing Request: #", RequestMethodsLog.push(chunk.toString()));
    console.log("Request Body:",RequestMethodsLog[RequestMethodsLog.length-1]);
-   //res.write(PageHtml);
-   next();
+   res.write(PageHtml);
 if(RequestMethodsLog.includes('error')){ throw new Error('error');}
-
-//if(RequestMethodsLog.length==10){RequestMethodsLog[(RequestMethodsLog.length-RequestM>
-
 callback(); //Successful callback
-
-
 }catch(err){
 callback(err);
 }
-  }
-});
-
-
-
-
-
-
-
-
-
+        }
+    });
 const DevFormat = morganlogs(function (tokens, req, res) {
 return [
     tokens.method(req, res),
@@ -88,12 +36,85 @@ return [
     tokens.status(req, res),
     tokens.res(req, res, 'content-length'), '-',
     tokens['response-time'](req, res), 'ms'
-  ].join(' ')},{stream:StreamObject});
+  ].join(' ')});
 
 
-app.use(morganlogs('DevFormat'));
-
-}
+*/
 
 
-module.exports = {Logger}
+
+
+
+
+
+
+
+///
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Register morgan middleware globally
+//app.use(morganlogs(DevFormat, { stream: StreamObject }));
+
+const RequestMethodsLog = [];
+const StreamObject = new Stream.Writable({
+  write: function (chunk, encoding, callback) {
+    try {
+      console.log(chunk.toString());
+      console.log("Writing Request: #", RequestMethodsLog.push(chunk.toString()));
+      console.log("Request Body:", RequestMethodsLog[RequestMethodsLog.length - 1]);
+      if (RequestMethodsLog.includes('error')) {
+        throw new Error('error');
+      }
+      callback(chunk); // Successful callback
+    } catch (err) {
+      callback(err);
+    }
+  }
+});
+
+
+
+const DevFormat = function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ');
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///
+
+module.exports = {Logger: () => [...RequestMethodsLog], 
+		  DevFormat: morganlogs(DevFormat, { stream: StreamObject })};
+
