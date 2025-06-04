50f43a6aea0d3a7dbea3eb44028e13e19b1b3d15
macimich-trove <michael.maciel@protonmail.com>
2025-06-04 02:12:36 -0400

    Stupid-ass house Keeping for the http request Logger

--- index.js
+++ index.js
@@ -3,42 +3,61 @@ const app = express();
 const cors = require('cors');
 const morganlogs = require('morgan');
 const cheerio = require('cheerio');
-
 const Stream = require('stream');
-
-
-
-const router = app.router;
-
 
 const port = process.env.PORT || 4002;
 
-const {Logger, DevFormat} = require('./middleware/logger-middleware.js');
-
-
-
-
+//const {} = require('./middleware/logger-middleware.js');
 var corsOptions = [{origin: 'localhost:3000', methods: ['GET', 'POST', 'PUT', 'DELETE']},{}]
 app.use(cors(...corsOptions));
 console.log(...corsOptions)
 
-app.use(Logger)
+
+const RequestMethodsLog = [];
+const StreamObject = new Stream.Writable({
+  write: function (chunk, encoding, callback) {
+    try {
+      console.log(chunk.toString());
+      console.log("Writing Request: #", RequestMethodsLog.push(chunk.toString()));
+      console.log("Request Body:", RequestMethodsLog[RequestMethodsLog.length - 1]);
+      if (RequestMethodsLog.includes('error')) {
+        throw new Error('error');
+      }
+      callback(); // Successful callback
+    } catch (err) {
+      callback(err);
+    }
+  }
+});
+
+
+
+const DevFormat = function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ');
+};
 
 
 
 
-app.use(morganlogs(DevFormat));
 
 
 
 
-router.get('/stats', (req, res) => {
 
+app.use(morganlogs(DevFormat, {stream: StreamObject}));
+
+
+
+
+app.get('/stats', (req, res) => {
 console.log("#STATS ROUTE#");
-const getLogs = Logger();
-
-const PageHtml = getLogs.map((Request) => `<li><b>${Request}</b></li>`).join(' ');
-
+const PageHtml = RequestMethodsLog.map((Request) => `<li><b>${Request}</b></li>`).join(' ');
   res.write(`<h1><b>#STATS ROUTE#</b></h1>`);
   res.write(PageHtml);
   // End the response to avoid hanging
@@ -49,45 +68,11 @@ const PageHtml = getLogs.map((Request) => `<li><b>${Request}</b></li>`).join(' '
 
 
 
-app.use(express.json()); 
+ 
 
-
-router.get('/dashboard', (req, res) => {
-res.write(JSON.stringify("DASHBOARD"));
-res.end();
-
-
-});
-
-router.get('/', (req, res) => {
-
-appdata = JSON.stringify("HOME ROUTE");
-//console.log(app);
-
-res.write(appdata);
-res.end();
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Send route stats and JSON payload
-//const PageHtml= RequestMethodsLog.map((x)=>x).join(' ');
-
-
+const router = app.router;
+router.get('/OtherRoute', (req, res) => { res.write(JSON.stringify("OtherRoute")); res.end(); });
+router.get('/', (req, res) => {res.send("HOME ROUTE"); });
 
 
 
